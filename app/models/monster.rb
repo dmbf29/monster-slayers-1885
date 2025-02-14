f7fd8ab5c7215fd2874b9dce1e81577615380b28
Douglas Berkley <douglasmberkley@gmail.com>
2025-02-14 10:48:39 +0900

    comment

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -3,5 +3,6 @@ class Monster < ApplicationRecord
   has_many :curses, dependent: :destroy
   has_many :powers, through: :curses
   validates :name, presence: true
+  # this allows us to save the key on the instance
   has_one_attached :photo # .photo -> .photo.key
 end
